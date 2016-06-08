ed38d6e701f7fed7a7297e78298c02ab487d616d
saquibamin <sqb.amin@gmail.com>
2016-06-08 10:29:31 -0700

    sleep

--- AntDateUtils/src/com/mkyong/core/utils/DateUtils.java
+++ AntDateUtils/src/com/mkyong/core/utils/DateUtils.java
@@ -5,7 +5,12 @@ import java.util.Date;
 public class DateUtils {
 
 	public static void main(String[] args) {
-
+try {
+	Thread.sleep(1000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
 		System.out.println(getLocalCurrentDate());
 		
 		
